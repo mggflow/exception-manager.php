eb478034d0540b62e20fb26e2f8ca11f40c14589
Deusagnis <deusagnis@gmail.com>
2023-03-08 21:43:04 +0300

    Add exception presetting for builder in ManageException.

--- src/Abstracted/ExceptionManager.php
+++ src/Abstracted/ExceptionManager.php
@@ -2,11 +2,13 @@
 
 namespace MGGFLOW\ExceptionManager\Abstracted;
 
+use MGGFLOW\ExceptionManager\Interfaces\UniException;
+
 abstract class ExceptionManager
 {
     protected static ExceptionBuilder $builder;
 
     abstract public static function setBuilder(ExceptionBuilder $builder): self;
 
-    abstract public static function build(): ExceptionBuilder;
+    abstract public static function build(?UniException $exception): ExceptionBuilder;
 }
--- src/ManageException.php
+++ src/ManageException.php
@@ -4,6 +4,7 @@ namespace MGGFLOW\ExceptionManager;
 
 use MGGFLOW\ExceptionManager\Abstracted\ExceptionManager;
 use MGGFLOW\ExceptionManager\Abstracted\ExceptionBuilder;
+use MGGFLOW\ExceptionManager\Interfaces\UniException;
 
 class ManageException extends ExceptionManager
 {
@@ -14,10 +15,11 @@ class ManageException extends ExceptionManager
         return new static();
     }
 
-    public static function build(): BuildException
+    public static function build(?UniException $exception = null): BuildException
     {
         if (empty(static::$builder)) static::setBuilder(new BuildException());
-        static::$builder->setUpException();
+
+        static::$builder->setUpException($exception);
 
         AccumulateException::accumulate(static::$builder->e());
 
